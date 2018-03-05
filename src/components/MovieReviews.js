edc27d5b7797602c0964784ddf75f8df58fb7a52
Michael Anderson <michanders@msn.com>
2018-03-05 20:56:54 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
-// Code MovieReviews Here
+import React from 'react';
+
+class MovieReviews extends React.Component {
+  constructor() {
+    super();
+
+    this.props = {
+      reviews: []
+    };
+  }
+
+  render() {
+    return (
+      <div className = "review-list">
+        {reviews.map(review =>
+          <div className="review">
+            <h2>Title: {review.display_title}</h2>
+            <p>Headline: {review.headline}</p>
+            <p>Opens: {review.opening_day}</p>
+        </div>
+      </div>
+    )
+  }
+};
