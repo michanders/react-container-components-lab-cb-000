7e7324dcbc5ea19c3ce0a843bb90b70d14c3ab97
Michael Anderson <michanders@msn.com>
2018-03-05 20:57:30 +0000

    automatically backed up by learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,24 @@
-// Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import LatestMovieReviewsContainer from './LatestMovieReviewsContainer'
+import MovieReviews from './MovieReviews'
+class SearchableMovieReviewsContainer extends React.Component {
+	constructor(){
+		super();
+
+		this.state = {
+			reviews: [],
+			searchTerm: ''
+		}
+	}
+
+
+	render(){
+		return (
+			<div className="searchable-movie-reviews">
+				<MovieReviews reviews={this.state.reviews} />
+			</div>
+		)
+	}
+}
+
+export default SearchableMovieReviewsContainer;
