061130bd912e07841d7819a13c85df5ce5d77a3c
Michael Anderson <michanders@msn.com>
2018-03-05 20:48:18 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -5,4 +5,20 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+ 
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount(){
+		fetch(URL)
+		.then((response) => response.json())
+		.then((data) => {
+			this.setState({reviews: data.results})
+		})
+	}
+}
