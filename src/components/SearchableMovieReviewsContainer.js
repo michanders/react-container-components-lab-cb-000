5160a9f02142e515b2617c0dd3a8c22a38d358af
Michael Anderson <michanders@msn.com>
2018-03-05 20:58:00 +0000

    automatically backed up by learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import LatestMovieReviewsContainer from './LatestMovieReviewsContainer'
 import MovieReviews from './MovieReviews'
-class SearchableMovieReviewsContainer extends React.Component {
+
+
+export default class SearchableMovieReviewsContainer extends React.Component {
 	constructor(){
 		super();
-
 		this.state = {
 			reviews: [],
-			searchTerm: ''
+			searchTerm: ""
 		}
 	}
 
-
 	render(){
 		return (
-			<div className="searchable-movie-reviews">
+			<div className = "searchable-movie-reviews">
 				<MovieReviews reviews={this.state.reviews} />
 			</div>
 		)
 	}
 }
 
-export default SearchableMovieReviewsContainer;
+
